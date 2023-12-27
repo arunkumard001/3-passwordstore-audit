94c920d72233c1def49b3c1f7794e3ca1aaa1630
= <=>
2023-12-27 06:57:40 -0500

    audit begin

--- src/PasswordStore.sol
+++ src/PasswordStore.sol
@@ -23,6 +23,7 @@ contract PasswordStore {
      * @notice This function allows only the owner to set a new password.
      * @param newPassword The new password to set.
      */
+
     function setPassword(string memory newPassword) external {
         s_password = newPassword;
         emit SetNetPassword();
